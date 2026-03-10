149d1a6002dc10affc931ebc0b3fcff536734507
Suzanne Daniel Thomas <suzanne.wkspace@gmail.com>
2026-03-10 06:47:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const form = document.getElementById("contactForm");
+const form = document.getElementById("messageForm");
 const successMsg = document.getElementById("successMsg");
 
-if (form) {  
+if (form) {
     form.addEventListener("submit", function(e) {
         e.preventDefault();
         const name = document.getElementById("name").value;
         const email = document.getElementById("email").value;
         const message = document.getElementById("message").value;
-        successMsg.textContent = "Thank you " + name + "! Message received.";
+        successMsg.textContent = "Thank you " + name + "! Message received. We will get back to you within 24-48 hours.";  
+        successMsg.style.marginTop = "15px";   /* pushes it below the button */
         form.reset();
     });
 }
